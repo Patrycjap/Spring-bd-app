a5a70b5a7d8f8d2725f6217fdfeebf094be48208
Pat <patrycjapostek@gmail.com>
2021-02-18 16:36:23 +0100

    users-part1

---
+++ src/main/java/pp/app/db/User/UserRepository.java
@@ -0,0 +1,11 @@
+package pp.app.db.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findAll();
+
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO user (id, name, surname, birthdate)
+VALUES
+(1, 'Jan', 'Kowalski', '1990-12-12'),
+(2, 'Alina', 'Abacka', '2000-12-12'),
+(3, 'Jaroslaw', 'Kowal', '1990-12-12');
